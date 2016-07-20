580d133375501387e836e04ee729c7737f3cbefb
esscbee <github@esscb.com>
2016-07-20 15:31:51 -0700

    Make the box bounce

--- PointFromSubScene/GameScene.swift
+++ PointFromSubScene/GameScene.swift
@@ -10,6 +10,9 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: -9.8)
+        resetPhysics()
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -19,7 +22,7 @@ class GameScene: SKScene {
                 let location = touch.locationInNode(self)
                 // print(touch)
                 print("location \(location)")
-                let nodePos = n.convertPoint(n.position, toNode: self)
+                let nodePos = self.convertPoint(n.position, fromNode: n.parent!)
                 print("nodePos: \(nodePos)")
                 
                 let size = CGSize(width: 20, height: 20)
@@ -33,6 +36,12 @@ class GameScene: SKScene {
                 bullet.runAction(seq)
             }
         }
+    }
+    func resetPhysics() {
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: frame)
+        self.physicsBody?.friction = 0
+        self.physicsBody?.restitution = 1
+        self.physicsBody?.contactTestBitMask = 0xffffffff
     }
    
     override func update(currentTime: CFTimeInterval) {
