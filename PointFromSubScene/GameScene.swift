798e8ff09109e913b8f23363ac32845461f6f692
esscbee <github@esscb.com>
2016-07-20 15:47:02 -0700

    Added impulses

--- PointFromSubScene/GameScene.swift
+++ PointFromSubScene/GameScene.swift
@@ -19,21 +19,29 @@ class GameScene: SKScene {
         if let n = childNodeWithName("//findMe") {
             for touch in touches {
                 print(n.position)
-                let location = touch.locationInNode(self)
-                // print(touch)
-                print("location \(location)")
-                let nodePos = self.convertPoint(n.position, fromNode: n.parent!)
-                print("nodePos: \(nodePos)")
-                
-                let size = CGSize(width: 20, height: 20)
-                let bullet = SKSpriteNode(color: UIColor.greenColor(), size: size)
-                bullet.position = nodePos
-                addChild(bullet)
-                
-                let move = SKAction.moveToX(frame.width, duration: 1)
-                let remove = SKAction.removeFromParent()
-                let seq = SKAction.sequence([ move, remove ])
-                bullet.runAction(seq)
+                let location = touch.locationInNode(n.parent!)
+                if n.containsPoint(location) {
+                    let dx = CGFloat(1000)
+                    let dy = CGFloat(5000)
+                    if location.x > n.position.x / 2 {
+                        n.physicsBody!.applyImpulse(CGVector(dx: -dx , dy: dy))
+                    } else {
+                        n.physicsBody!.applyImpulse(CGVector(dx: dx , dy: dy))
+                    }
+                    
+                } else {
+                    let nodePos = self.convertPoint(n.position, fromNode: n.parent!)
+                    
+                    let size = CGSize(width: 20, height: 20)
+                    let bullet = SKSpriteNode(color: UIColor.greenColor(), size: size)
+                    bullet.position = nodePos
+                    addChild(bullet)
+                    
+                    let move = SKAction.moveToX(frame.width, duration: 1)
+                    let remove = SKAction.removeFromParent()
+                    let seq = SKAction.sequence([ move, remove ])
+                    bullet.runAction(seq)
+                }
             }
         }
     }
